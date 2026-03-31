dc45088d0bd5ea9f8f43a370e88bad72a3e01c2b
phtnj7 <pietroheenrique85@gmail.com>
2026-03-31 14:00:39 -0300

    Add files via upload

---
+++ PW2/IDADE/idade.js
@@ -0,0 +1,4 @@
+var nome = (prompt("Digite seu nome: "))
+var idade = parseInt(prompt("Digite seu ano de nascimento: "))
+atual = 2026 - idade
+console.log("Olá", nome,"sua idade em 2026 é: ", atual)
