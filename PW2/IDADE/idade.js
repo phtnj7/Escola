9bef5971bde0d29db28714a518aeb741a78402d0
phtnj7 <pietroheenrique85@gmail.com>
2026-03-31 14:15:16 -0300

    Delete PW2 directory

--- PW2/IDADE/idade.js
+++
@@ -1,4 +0,0 @@
-var nome = (prompt("Digite seu nome: "))
-var idade = parseInt(prompt("Digite seu ano de nascimento: "))
-atual = 2026 - idade
-console.log("Olá", nome,"sua idade em 2026 é: ", atual)
